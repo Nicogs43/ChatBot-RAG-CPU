46e50c9fbb3ebeeb102937b79b1b1e39dff49bf8
Nicogs43 <nico@guainazzo.it>
2024-12-16 10:20:52 +0100

    fix a issue in download_embedding_model

--- download_embedding_model.py
+++ download_embedding_model.py
@@ -7,5 +7,5 @@ path_to_model = Path(f"models/{model_id}")
 print(path_to_model)
 
 export_command_base = "optimum-cli export openvino --model {} --task feature-extraction".format(model_id) + " " + str(path_to_model)
-subprocess.run(export_command_base, shell=True)
+subprocess.run(export_command_base)
 
